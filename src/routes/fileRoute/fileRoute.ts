fe6f16acd1d1e4a6c74701c56ec35d011cb4d572
mohak <c.mohak2004@gmail.com>
2025-04-02 21:54:29 +0530

    aws based file upload done (v2 based, needs update)

--- src/routes/fileRoute/fileRoute.ts
+++ src/routes/fileRoute/fileRoute.ts
@@ -1,10 +1,56 @@
 import express from "express";
+import AWS from "aws-sdk";
+import multer from "multer"; 
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const bucket_name = process.env.AWS_S3_BUCKET_NAME as string
+const accessKey = process.env.AWS_ACCESS_KEY_ID as string
+const s3 = new AWS.S3({
+    accessKeyId : accessKey ,
+    secretAccessKey :process.env.AWS_SECRET_ACCESS_KEY ,
+    region : process.env.AWS_REGION
+})
+
+const upload = multer({storage : multer.memoryStorage()})
 
 export const fileRouter = express.Router();
 
-fileRouter.post("/upload", (req : any,res: any ) => {
+fileRouter.post("/upload", upload.single('file'), async (req : any,res: any ) => {
+ if(!req.file) res.status(400).json({ msg : "no file found"});
+
+ const {originalname ,mimetype ,buffer } = req.file
+ const s3Key = `uploads/${Date.now()}_${originalname}` ;
+
+ const params = {
+    Bucket : bucket_name,
+    Key : s3Key ,
+    Body : buffer,
+    ContentType : mimetype
+ }
+
+ try {
+    const response = await s3.upload(params).promise(); 
+
+    if (response) {
+      console.log("uploaded")
+    }
+
+    res.json({
+      msg : "uploaded", 
+      response,
+      s3Key
+    })
+
+ } catch (error) {
+   console.error("S3 upload error:", error);
+   res.status(500).json({ msg: "Upload failed" });
+ }
 
 })
+
+
 
 fileRouter.get("/download/:fileId", (req: any, res: any) => {
 
